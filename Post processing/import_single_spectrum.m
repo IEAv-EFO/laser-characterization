641c79726c174db3d63f3bb0ee293e133fefed9b
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2022-11-16 12:50:00 -0300

    import single spec

--- Post processing/import_single_spectrum.m
+++ Post processing/import_single_spectrum.m
@@ -1,9 +1,8 @@
 % Script to import one lvm file containing one spectrum
 % into matlab workspace
 
-filename = 'LabVIEW Data 8 .lvm'
-path = 'C:\Users\User\Meu Drive\ITA\Data\2 - Laser diode characterization\data\acquisition3_50mA-650mA_50mAstep'
-% path = 'C:\Users\User\Meu Drive\ITA\Data\10 - Gyroscope\IFOG 1550nm\2022_04_12\acq7_50_400mA_dbm_nm_20c_span_200nm';
+filename = 'LabVIEW Data 8 .lvm';
+path = uigetdir();
 initial_path = pwd;
 cd(path)
 data = importdata([path,'\',filename]);
