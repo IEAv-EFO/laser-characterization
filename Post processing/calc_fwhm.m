f8e43a110aa5a66f89f665349305165e04b494cf
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2022-11-28 10:16:44 -0300

    style - tab inside functions

--- Post processing/calc_fwhm.m
+++ Post processing/calc_fwhm.m
@@ -5,54 +5,54 @@ function fwhm = calc_fwhm(x,y,db)
 % 
 % db = 1 --> measurement in dBm
 % db = 0 --> measurement in mW
-
-% Find the half max value.
-if db == 1
-    mid = max(y) -3; 
-elseif db == 0
-    mid = max(y) /2; 
-end
-
-% find first point
-[ind1, pow1] = find(y >= mid, 1, 'first');
-
-% interpolate if this is not the exact match
-if pow1 ~= mid
-    ind1_1 = ind1-1;
-    if ind1_1 == 0
-        ind1_1 = 1;
-    end
-
-    xx = [x(ind1) x(ind1_1)];
-    yy = [y(ind1) y(ind1_1)];
-    c = [[1; 1]  xx(:)] \yy(:);
-    m = c(2);
-    b = c(1);
     
-    lamb1 = (mid-b)/m;
-else
-    lamb1 = x(ind1);
-end
-
-% find second point
-[ind2, pow2] = find(y >= mid, 1, 'last');
-
-if pow2 ~= mid
-    ind2_1 = ind2+1;
-    if ind2_1 > length(y)
-        ind2_1 = length(y);
+    % Find the half max value.
+    if db == 1
+        mid = max(y) -3; 
+    elseif db == 0
+        mid = max(y) /2; 
     end
-
-    xx = [x(ind2) x(ind2_1)];
-    yy = [y(ind2) y(ind2_1)];
-    c = [[1; 1]  xx(:)] \yy(:);
-    m = c(2);
-    b = c(1);
     
-    lamb2 = (mid-b)/m;
-else
-    lamb2 = x(ind2);
-end
-
-fwhm = lamb2 - lamb1;
+    % find first point
+    [ind1, pow1] = find(y >= mid, 1, 'first');
+    
+    % interpolate if this is not the exact match
+    if pow1 ~= mid
+        ind1_1 = ind1-1;
+        if ind1_1 == 0
+            ind1_1 = 1;
+        end
+    
+        xx = [x(ind1) x(ind1_1)];
+        yy = [y(ind1) y(ind1_1)];
+        c = [[1; 1]  xx(:)] \yy(:);
+        m = c(2);
+        b = c(1);
+        
+        lamb1 = (mid-b)/m;
+    else
+        lamb1 = x(ind1);
+    end
+    
+    % find second point
+    [ind2, pow2] = find(y >= mid, 1, 'last');
+    
+    if pow2 ~= mid
+        ind2_1 = ind2+1;
+        if ind2_1 > length(y)
+            ind2_1 = length(y);
+        end
+    
+        xx = [x(ind2) x(ind2_1)];
+        yy = [y(ind2) y(ind2_1)];
+        c = [[1; 1]  xx(:)] \yy(:);
+        m = c(2);
+        b = c(1);
+        
+        lamb2 = (mid-b)/m;
+    else
+        lamb2 = x(ind2);
+    end
+    
+    fwhm = lamb2 - lamb1;
 end
--- Post processing/calc_mean_wavelength.m
+++ Post processing/calc_mean_wavelength.m
@@ -4,13 +4,13 @@ function mean_wav = calc_mean_wavelength(lamb, power, db)
 % mean_wav = calc_mean_wav(lamb,power)
 % db = 1 --> dBm or dBm/nm
 % db = 0 --> mW
-
-if db == 1
-    power = 10.^((power)/10); % [mW]
-end
-dlamb = diff(lamb);
-
-mean_wav = sum(power(1:end-1).*lamb(1:end-1).*dlamb)/...
-           sum(power(1:end-1).*dlamb);
+    
+    if db == 1
+        power = 10.^((power)/10); % [mW]
+    end
+    dlamb = diff(lamb);
+    
+    mean_wav = sum(power(1:end-1).*lamb(1:end-1).*dlamb)/...
+               sum(power(1:end-1).*dlamb);
 
 end
